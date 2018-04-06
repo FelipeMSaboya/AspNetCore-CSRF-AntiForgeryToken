b926ac1fad76bb63f8c857495140e1c834a85c53
Felipe Monteiro <felipe.m.saboya@outlook.com>
2018-04-06 15:14:38 -0300

    (Test for more complex articles)

---
+++ AspNetCore-CSRF-AntiForgeryToken/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCore_CSRF_AntiForgeryToken.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCore_CSRF_AntiForgeryToken.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(TestViewModel viewModel)
+        {
+            // Your code here!
+
+            return await Task.Run(() => View());
+        }
+    }
+}
---
+++ AspNetCore-CSRF-AntiForgeryToken/Models/TestViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AspNetCore_CSRF_AntiForgeryToken.Models
+{
+    public class TestViewModel
+    {
+    }
+}
